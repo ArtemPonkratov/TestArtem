d9a6558959578219264f68083db5c3ff0196743e
ArtemPonkratov <artemka.g@mail.ru>
2023-04-05 16:39:54 +0200

    test

---
+++ projest/main.js
@@ -0,0 +1,3 @@
+console.log("Здраствуйте, Тимур! Я учусь программировать на JavaScript.");
+console.log("Результат выражения 2+2 равен: ", 2+2);
+console.log("%cЗаголовок", "font-size: 24px; color: blue;");
